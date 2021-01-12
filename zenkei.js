474409a26de88f94a06446585ca9679f01448092
TOS <ostermaier.timm@gmail.com>
2021-01-12 22:41:01 +0100

    ADD new Version of Zenkei.js
    ADD
    * communication pipeline
    
    CHANGE
    * new render method

--- zenkei.js
+++ zenkei.js
@@ -1,7 +1,7 @@
 /**
- * zenkei.js (Japanse 前景, German: Vordergrund) is a tool to create easy micro frontend applications
- * 
- * @version 0.1
+ * zenkei.js (Japanese 前景, German: Vordergrund) is a tool implement micro frontends
+ *  
+ * @version 0.5
  * @developer Timm Ostermaier
  * @license MIT
  */
@@ -12,6 +12,7 @@ class Zenkei {
      * <url, frontend> 
      */
     registeredFrontends = new Map();
+    registeredCommunicationEvents = [];
 
     constructor() {
 
@@ -21,67 +22,150 @@ class Zenkei {
      * register a new frontend  
      */
     register(url, elements, elementToAppend) {
-        let frontend = new Frontend(url, elements, elementToAppend);
+        let frontend = new MicroFrontend(url, elements, elementToAppend);
         this.registeredFrontends.set(url, frontend);
-        
+
         return frontend;
     }
 
-    render(frontend){
+    /**
+     * render a frontend
+     * @param frontend to render 
+     */
+    render(frontend) {
         frontend.render();
     }
 
-    initFrontend(url,elementToAppend){
+    /**
+     * 
+     * @param url to request frontend
+     * @param elementToAppend add micro frontend to that element
+     */
+    initFrontend(url, elementToAppend) {
+        console.debug("render frontend for", url);
         this.requestFrontend(url)
-        .then((data) => {
-            this.render(   this.register(url,data, elementToAppend))
-        }).catch((error) => {
-            console.log(error);
-        });
+            .then((data) => {
+                this.render(this.register(url, data, elementToAppend))
+            }).catch((error) => {
+                console.error("error in initFrontend(), error: ", error);
+            });
     }
 
+    /**
+     * request frontend from server
+     * @param url to request frontend
+     */
     requestFrontend(url) {
 
-       return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
             let request = new XMLHttpRequest(url);
-            request.open("GET", productAddress, true);
+            request.open("GET", url, true);
 
             request.onload = () => {
                 if (request.readyState == 4 && request.status == 200) {
                     resolve(request.responseText);
                 } else {
                     reject("error")
-                    console.warn("requesting frontend failed",this)
+                    console.warn("requesting frontend failed", this)
                 }
             }
             request.send();
         })
     }
+
+    /**
+     * register communication event for cross micro frontend communication
+     * @param {*} eventName : name (string) for event
+     * @param {*} eventFunction  : function to register
+     */
+    registerCommunicationEvent(eventName, eventFunction) {
+
+        try {
+           this.registeredCommunicationEvents[eventName] = eventFunction;
+        } catch (e) {
+            console.error(
+                "error in registering communication function", eventFunction,
+                "Error", e)
+        }
+
+    }
+
+    executeCommunicationEvent(event, ...n) {
+
+        console.log("executeCommunicationEvent()", event)
+        try {
+
+            if (this.registeredCommunicationEvents[event] != null) {
+                this.registeredCommunicationEvents[event](n);
+            }
+        } catch (e) {
+            console.error(
+                "error in execution of", event,
+                "Error", e);
+        }
+    }
+
 }
 
 /**
  * class for a specifc frontend part
  */
-class Frontend {
+class MicroFrontend extends HTMLElement {
 
     htmlElementsString = "";
     htmlElementsDom = [];
     url = "";
     parser = new DOMParser();
-    
+    microFrontend = null;
+    initScripts = null;
+
+    communicationFunctions = []
+
     constructor(url, elements, elementToAppend) {
+        super();
         this.url = url;
         this.htmlElementsString = elements;
         this.parseToHtml(this.htmlElementsString);
         this.rootElement = elementToAppend;
     }
 
+    /**
+     * create micro-frontend dom node and renders childs element into them
+     */
     render() {
-        this.rootElement.appendChild(this.htmlElementsDom.documentElement)
-    }  
-
-    parseToHtml(){
-        this.htmlElementsDom = this.parser.parseFromString(this.htmlElementsString,'text/html');
+        this.innerHTML = this.htmlElementsString;
+        this.rootElement.appendChild(this);
+        this.createScript();
     }
+
+    /**
+     * parse htmlString to HTMl-Document
+     */
+    parseToHtml() {
+        this.htmlElementsDom = this.parser.parseFromString(this.htmlElementsString, 'text/html');
+    }
+
+    connectedCallback() {
+        console.log("connect frontend")
+    }
+
+    /**
+     * enable micro frontend scripts
+     */
+    createScript() {
+        if (this.htmlElementsDom.getElementsByClassName("microfrontend_script") != null) {
+
+            for (let el of this.htmlElementsDom.getElementsByClassName("microfrontend_script")) {
+                let script = document.createElement("script")
+                script.setAttribute("type", 'module');
+                script.src = el.src;
+                this.appendChild(script);
+            }
+        }
+
+    }
+
 }
+
+window.customElements.define('micro-frontend', MicroFrontend);
